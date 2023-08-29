8bfa8d8aaed3204320063ed6882fd6561c9440c9
pedrotonin <phavilat@hotmail.com>
2023-08-28 22:06:26 -0300

    desenvolvimento inicial da feature de navegação do app, commit para teste

--- App.js
+++ App.js
@@ -1,18 +1,27 @@
-import { useCallback } from 'react';
-import { View } from 'react-native';
-import { useFonts } from 'expo-font';
-import * as SplashScreen from 'expo-splash-screen';
-import Cadastrar from './src/components/pages/cadastrar/Cadastrar';
-import Login from './src/components/pages/login/Login';
-import Navigation from './navigation'
+import { useCallback } from "react";
+import { View } from "react-native";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { StatusBar } from "expo-status-bar";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+import Home from "./src/components/pages/home/Home";
+import Scan from "./src/components/pages/scan/Scan";
+import Search from "./src/components/pages/search/Search";
+import Colors from "./src/css/default/Colors";
+import Cadastrar from "./src/components/pages/cadastrar/Cadastrar";
+import Login from "./src/components/pages/login/Login";
 
 SplashScreen.preventAutoHideAsync();
 
-export default function App() {
+const Stack = createNativeStackNavigator();
+const BottomTab = createBottomTabNavigator();
 
+export default function App() {
   //carregar as fontes
   const [fontsLoaded] = useFonts({
-    'Bebas-Neue': require('./assets/fonts/BebasNeue-Regular.ttf'),
+    "Bebas-Neue": require("./assets/fonts/BebasNeue-Regular.ttf"),
   });
 
   const onLayoutRootView = useCallback(async () => {
@@ -27,9 +36,13 @@ export default function App() {
 
   return (
     <View style={{ flex: 1 }} onLayout={onLayoutRootView}>
-      <Cadastrar />
-      <Navigation />
+      <StatusBar style={Colors.type === "darkmode" ? "light" : "dark"} />
+
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={Home} />
+        <Tab.Screen name="Search" component={Search} />
+        <Tab.Screen name="Scan" component={Scan} />
+      </Tab.Navigator>
     </View>
   );
-  
 }
--- src/css/default/Colors.js
+++ src/css/default/Colors.js
@@ -1,21 +1,23 @@
-const ColorsWhite = {
-   backgroundColor: 'white',  
-   primaryButtonColor: '#2E6025',
-   primaryFontColor: 'black',
-   primaryFontColorButton: 'white',
-   white: 'white',
-   error: '#F77272'
-}
+const ColorsLightMode = {
+  type: "lightmode",
+  backgroundColor: "white",
+  primaryButtonColor: "#2E6025",
+  primaryFontColor: "black",
+  primaryFontColorButton: "white",
+  white: "white",
+  error: "#F77272",
+};
 
-const ColorsBlack = {
-   backgroundColor: '#2D2D2D',
-   primaryButtonColor: '#2E6025',
-   primaryFontColor: 'white',
-   primaryFontColorButton: 'white',
-   white: 'white',
-   error: '#F77272'
-}
+const ColorDarkMode = {
+  type: "darkmode",
+  backgroundColor: "#2D2D2D",
+  primaryButtonColor: "#2E6025",
+  primaryFontColor: "white",
+  primaryFontColorButton: "white",
+  white: "white",
+  error: "#F77272",
+};
 
-const Colors = ColorsBlack
+const Colors = ColorDarkMode;
 
-export default Colors
+export default Colors;
