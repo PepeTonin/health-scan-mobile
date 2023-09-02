1542a7b39c2fd8f2062183119566c75e9da33bde
pedrotonin <phavilat@hotmail.com>
2023-09-01 21:44:20 -0300

    atualizacao dos arquivos

--- App.js
+++ App.js
@@ -1,21 +1,10 @@
 import { useCallback } from "react";
 import { View } from "react-native";
 import { useFonts } from "expo-font";
-<<<<<<< Updated upstream
-=======
-import * as SplashScreen from "expo-splash-screen";
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
->>>>>>> Stashed changes
 import { StatusBar } from "expo-status-bar";
 import * as SplashScreen from "expo-splash-screen";
 
-import Home from "./src/components/pages/home/Home";
-import Scan from "./src/components/pages/scan/Scan";
-import Search from "./src/components/pages/search/Search";
 import Colors from "./src/css/default/Colors";
-import Cadastrar from "./src/components/pages/cadastrar/Cadastrar";
-import Login from "./src/components/pages/login/Login";
 import Routes from "./src/routes/Routes";
 
 SplashScreen.preventAutoHideAsync();
@@ -36,7 +25,6 @@ export default function App() {
     return null;
   }
 
-<<<<<<< Updated upstream
   const isLogged = false;
 
   return (
@@ -46,19 +34,5 @@ export default function App() {
         <Routes />
       </View>
     </>
-=======
-  return (
-    <View style={{ flex: 1 }} onLayout={onLayoutRootView}>
-      <StatusBar style={Colors.type === "darkmode" ? "light" : "dark"} />
-      <NavigationContainer>
-
-        <BottomTab.Navigator>
-          <BottomTab.Screen name="Home" component={Home} />
-          <BottomTab.Screen name="Search" component={Search} />
-          <BottomTab.Screen name="Scan" component={Scan} />
-        </BottomTab.Navigator>
-      </NavigationContainer>
-    </View>
->>>>>>> Stashed changes
   );
 }
