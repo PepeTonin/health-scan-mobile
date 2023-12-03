a26e508fe4bc50bc1b0557c924bf334cfae01c33
PepeTonin <phavilat@hotmail.com>
2023-12-03 18:44:41 -0300

    alterei o ip base

--- src/service/default/baseUrlService.js
+++ src/service/default/baseUrlService.js
@@ -1,9 +1,5 @@
-//Endereço de api
-const openFoodFacts = "https://world.openfoodfacts.org/api/v4/"
-
 //Endereço do servidor
-const dev = "http://192.168.1.14:8080/" 
+const dev = "http://172.31.208.1:8080/" 
 const prod = "https://helthscan.pagekite.me/"
 
 export const defaultUrl = dev;
-export const apiProduto = openFoodFacts;
